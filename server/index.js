31917d656983efd1d4f0f2fceab5baeee7b311f2
Jonty <ramkrishna250325@gmail.com>
2026-05-25 17:14:57 +0530

    update cors for deployment

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// Middleware
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
